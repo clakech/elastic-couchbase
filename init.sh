dc42231c82e07ed42fbc052c2dcd623326ae57e8
clakech <cyril.lakech@adeoservices.com>
2015-09-28 18:15:39 +0200

    wait for complete startup before configuration

--- init.sh
+++ init.sh
@@ -1,16 +1,25 @@
 #!/bin/bash
 
+wait_for_start() {
+    "$@"
+    while [ $? -ne 0 ]
+    do
+        echo 'waiting for elastic to start'
+        sleep 1
+        "$@"
+    done
+}
+
+
 echo "launch elastic"
 /elasticsearch/bin/elasticsearch &
 
-sleep 15s
+wait_for_start echo "create couchbase template"
+wait_for_start cd /elasticsearch
+wait_for_start curl -X PUT http://localhost:9200/_template/couchbase -d @plugins/transport-couchbase/couchbase_template.json 
 
-echo "create couchbase template"
-cd /elasticsearch
-curl -X PUT http://localhost:9200/_template/couchbase -d @plugins/transport-couchbase/couchbase_template.json 
-
-echo "create data and cache indexes"
-curl -X PUT http://localhost:9200/data
+wait_for_start echo "create data index"
+wait_for_start curl -X PUT http://localhost:9200/data
 	
 wait
 
